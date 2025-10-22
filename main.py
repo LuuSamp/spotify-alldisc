e0397fc0b5d9d8b2d01ee5a55098b1e123554877
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2025-10-22 13:48:36 -0300

    refactor: readability and error handling

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ dotenv.load_dotenv()
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from spotipy.exceptions import SpotifyOauthError, SpotifyException
+import sys
 
 def get_ids(item_list):
     ids = []
@@ -11,21 +13,36 @@ def get_ids(item_list):
 
     return ids
 
-scope = 'playlist-modify-private'
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-artist_url = input("Artist URL: ")
-artist_albums = sp.artist_albums(artist_url)["items"]
-artist_name = sp.artist(artist_url)["name"]
+def main():
+    scope = 'playlist-modify-private'
+    try:
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    except SpotifyOauthError as err:
+        print(err)
+        print()
+        print("Error during Spotify authentication. Please check your environment variables.")
+        sys.exit(1)
 
-playlist_name = f"{artist_name} Discography"
-
-playlist = sp.user_playlist_create(user=sp.me()["id"], name=playlist_name, public=False)
-
-for album in artist_albums:
-    album_tracks = sp.album_tracks(album["id"])["items"]
-    tracks_ids = get_ids(album_tracks)
-    sp.playlist_add_items(playlist["id"], tracks_ids)
+    artist_url = input("Artist URL or ID: ")
+    try:
+        artist_albums = sp.artist_albums(artist_url)["items"]
+        artist_name = sp.artist(artist_url)["name"]
+    except SpotifyException as err:
+        print(err)
+        print()
+        print(f"Error fetching artist information. URL '{artist_url}' may be invalid.")
+        sys.exit(1)
     
-    
+    playlist_name = f"{artist_name} Discography"
 
+    playlist = sp.user_playlist_create(user=sp.me()["id"], name=playlist_name, public=False)
+
+    for album in artist_albums:
+        album_tracks = sp.album_tracks(album["id"])["items"]
+        tracks_ids = get_ids(album_tracks)
+        sp.playlist_add_items(playlist["id"], tracks_ids)
+        
+if __name__ == "__main__":
+    main()
+    
